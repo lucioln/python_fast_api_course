7a84bd6a776b620c564b7a6d8cef46e6a7d16919
Lucio Noleto <lucio.pdcase@emap.ma.gov.br>
2025-01-21 15:20:57 -0300

    Adicionada a primeira migração com Alembic. Criada tabela de usuários.

---
+++ tests/test_db.py
@@ -0,0 +1,22 @@
+from dataclasses import asdict
+
+from sqlalchemy import select
+
+from fast_zero.models import User
+
+
+def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User) as time:
+        new_user = User(username='foo', password='bar', email='foo@bar.com')
+        session.add(new_user)
+        session.commit()
+
+    user = session.scalar(select(User).where(User.username == 'foo'))
+
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'foo',
+        'password': 'bar',
+        'email': 'foo@bar.com',
+        'created_at': time,
+    }
